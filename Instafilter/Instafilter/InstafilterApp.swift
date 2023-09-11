65efb52bb73e47081eaddeeb87813c938dfc8078
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-09-11 19:55:04 -0300

    Initial Commit of Instafilter

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Isaque da Silva on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
