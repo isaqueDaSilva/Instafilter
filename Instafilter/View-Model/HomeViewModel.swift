b7f8600ac3f2ec6becf2173b5fe926949ea56366
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-09-15 23:59:19 -0300

    Some updates

--- Instafilter/View-Model/HomeViewModel.swift
+++ Instafilter/View-Model/HomeViewModel.swift
@@ -5,58 +5,26 @@
 //  Created by Isaque da Silva on 14/09/23.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import Foundation
 import SwiftUI
 
 extension HomeView {
     class HomeViewModel: ObservableObject {
-        let context = CIContext()
+        let manager = FilterManager()
         
-        @Published var image: Image?
-        @Published var inputImage: UIImage?
-        @Published var filterIntensity: Double = 0.5
         @Published var showingfilterIntensity: Bool = false
         @Published var showingPhotoLibrary = false
-        @Published var currentFilter = CIFilter.sepiaTone()
         
-        func changeFilter() {
-            
-        }
-        
-        func save() {
-            
+        func image() -> Image? {
+            manager.image
         }
         
         func loadImage() {
-            guard let inputImage = inputImage else { return }
-            let beginImage = CIImage(image: inputImage)
-            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-            applyFilter()
+            manager.loadImage()
         }
         
         func applyFilter() {
-            let inputKeys = currentFilter.inputKeys
-            
-            if inputKeys.contains(kCIInputIntensityKey) {
-                currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
-            }
-            
-            if inputKeys.contains(kCIInputRadiusKey) {
-                currentFilter.setValue(filterIntensity, forKey: kCIInputRadiusKey)
-            }
-            
-            if inputKeys.contains(kCIInputScaleKey) {
-                currentFilter.setValue(filterIntensity, forKey: kCIInputScaleKey)
-            }
-            
-            guard let outputImage = currentFilter.outputImage else { return }
-            
-            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
-                let uiImage = UIImage(cgImage: cgImage)
-                image = Image(uiImage: uiImage)
-            }
+            manager.applyFilter()
         }
     }
 }
